feca2cd8dd262a7d0b15a0ac52ddbe7488ad1807
projectxtirta <ardiytriplesix@gmail.com>
2026-09-15 18:50:18 +0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://vxwbtuetshwrifnizuer.supabase.co/rest/v1/'
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_23X7WCdjLuPI1bFZQnVarA_y0aU_YuO'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://ghmdupxajrxroplbrziy.supabase.co/rest/v1//'
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_Wf7wSRZ1mvYacQPCVTKKLA_oBrP9IEd'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
